7f74f8e20af9b3182272d2cc3854c18211da34ee
De_miner <davidoukitel123@gmail.com>
2025-04-06 23:49:26 +0300

    Добавлены методы GET, POST, PUT для компаний (обращения по id)
    Добавлены комментарии

--- Backend/Controllers/CompanyController.cs
+++ Backend/Controllers/CompanyController.cs
@@ -1,19 +1,19 @@
 ﻿using DataBase;
 using DataBase.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Controllers
 {
     [Route("/api/[controller]")]
     [ApiController]
-    public class ManagerController : Controller
+    public class CompanyController : Controller
     {
         private readonly HttpRequest _request;
         private readonly HttpResponse _response;
         private readonly PriazovContext _db;
 
-        public ManagerController(HttpContext context, PriazovContext db)
+        public CompanyController(HttpContext context, PriazovContext db)
         {
             _request = context.Request;
             _response = context.Response;
@@ -21,46 +21,55 @@ namespace Controllers
         }
 
         /// <summary>
-        /// Выводит менеджера по его id
+        /// Выводит список компаний
+        /// </summary>
+        [HttpGet]
+        public async Task GetCompany()
+        {
+            await _db.Companies.ToListAsync();
+        }
+
+        /// <summary>
+        /// Выводит компанию по её id
         /// </summary>
         [HttpGet("{id}")]
-        public async Task GetManager(Guid? id)
+        public async Task GetCompany(Guid? id)
         {
-            Manager? user = _db.Managers.FirstOrDefault((u) => u.Id == id);
-            // если пользователь найден, отправляем его
+            Company? user = _db.Companies.FirstOrDefault((u) => u.Id == id);
+            // если компания найдена, отправляем её
             if (user != null)
             {
                 _response.StatusCode = 200;
                 await _response.WriteAsJsonAsync(user);
             }
-            // если не найден, отправляем статусный код и сообщение об ошибке
+            // если не найдена, отправляем статусный код и сообщение об ошибке
             else
             {
                 _response.StatusCode = 404;
-                await _response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+                await _response.WriteAsJsonAsync(new { message = "Компания не найдена" });
             }
         }
 
         /// <summary>
-        /// Создаёт нового менеджера
+        /// Создаёт новую компанию
         /// </summary>
         [HttpPost]
-        public async Task PostManager()
+        public async Task PostCompany()
         {
             try
             {
-                // получаем данные пользователя
-                var user = await _request.ReadFromJsonAsync<Manager>();
+                // получаем данные компании
+                var user = await _request.ReadFromJsonAsync<Company>();
                 if (user != null)
                 {
-                    // устанавливаем id для нового пользователя
+                    // устанавливаем id для новой компании
                     user.Id = Guid.NewGuid();
-                    // добавляем пользователя в список
-                    await _db.Managers.AddAsync(user);
+                    // добавляем компанию в список
+                    await _db.Companies.AddAsync(user);
                     await _db.SaveChangesAsync();
                     await _response.WriteAsJsonAsync(user);
                 }
-                // если не найден, отправляем статусный код и сообщение об ошибке
+                // если не найдена, отправляем статусный код и сообщение об ошибке
                 else
                 {
                     throw new Exception("Некорректные данные");
@@ -74,20 +83,20 @@ namespace Controllers
         }
 
         /// <summary>
-        /// Изменяет менеджера по id
+        /// Изменяет компанию по id
         /// </summary>
         [HttpPut("{id}")]
-        public async Task PutManager(Guid? id)
+        public async Task PutCompany(Guid? id)
         {
             try
             {
-                // получаем данные пользователя
-                var userData = await _request.ReadFromJsonAsync<Manager>();
+                // получаем данные компании
+                var userData = await _request.ReadFromJsonAsync<Company>();
                 if (userData != null)
                 {
-                    // получаем данные пользователя из базы данных
-                    Manager? user = _db.Managers.FirstOrDefault((u) => u.Id == id);
-                    // если пользователь найден, изменяем его данные
+                    // получаем данные компании из базы данных
+                    Company? user = _db.Companies.FirstOrDefault((u) => u.Id == id);
+                    // если компания найдена, изменяем его данные
                     if (user != null)
                     {
                         _response.StatusCode = 200;
@@ -97,14 +106,14 @@ namespace Controllers
                         await _db.SaveChangesAsync();
                         await _response.WriteAsJsonAsync(user);
                     }
-                    // если не найден, отправляем статусный код и сообщение об ошибке
+                    // если не найдена, отправляем статусный код и сообщение об ошибке
                     else
                     {
                         _response.StatusCode = 404;
-                        await _response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+                        await _response.WriteAsJsonAsync(new { message = "Компания не найден" });
                     }
                 }
-                // если не найден, отправляем статусный код и сообщение об ошибке
+                // если не найдена, отправляем статусный код и сообщение об ошибке
                 else
                 {
                     throw new Exception("Некорректные данные");
